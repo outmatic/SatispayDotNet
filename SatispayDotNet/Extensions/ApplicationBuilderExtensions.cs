daf5a4b7f61cff756fcb67c7d0da58bb5ec02208
Alessandro Petrelli <alessandro.petrelli@outlook.com>
2021-05-10 22:31:59 +0200

    fix registration

--- SatispayDotNet/Extensions/ApplicationBuilderExtensions.cs
+++ SatispayDotNet/Extensions/ApplicationBuilderExtensions.cs
@@ -1,15 +1,23 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
 using SatispayDotNet.Handlers;
 
 namespace SatispayDotNet.Extensions
 {
     public static class ApplicationBuilderExtensions
     {
-        public static IServiceCollection AddSatispayClient(this IServiceCollection services, string keyId, string privateKey)
+        public static IServiceCollection AddSatispayClient(this IServiceCollection services, string keyId, string privateKey, bool production)
         {
-            services.AddTransient(s => new SatispayRequestSigningDelegatingHandler(keyId, privateKey));
-            services.AddHttpClient<SatispayClient>()
-                .AddHttpMessageHandler(s => s.GetService<SatispayRequestSigningDelegatingHandler>());
+            services.AddHttpClient("SatispayClient")
+                .AddHttpMessageHandler(c => new SatispayRequestSigningDelegatingHandler(keyId, privateKey));
+
+            services.AddTransient(c =>
+            {
+                var clientFactory = c.GetRequiredService<IHttpClientFactory>();
+                var httpClient = clientFactory.CreateClient("SatispayClient");
+
+                return new SatispayClient(httpClient, production);
+            });
 
             return services;
         }
